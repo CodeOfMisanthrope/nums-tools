51c8f2b1c02c9315d54e0cc56295684c422a91d8
Ilya <iosdsbb@gmail.com>
2025-04-30 13:50:15 +0300

    feat: removed FloatNum

--- src/float.rs
+++ src/float.rs
@@ -1,45 +1,23 @@
-pub struct FloatNum {
-    sign: f32,
-    exponent: f32,
-    mantissa: f32,
-    val: f32,
-}
+const BIAS: i32 = 127; // <1>
+const RADIX: f32 = 2.0; // <1>
 
-impl FloatNum {
-    const BIAS: i32 = 127;
-    const RADIX: f32 = 2.0;
+pub fn decode(sign: u32, exponent: u32, fraction: u32) -> (f32, f32, f32) {
+    let signed_1 = (-1.0_f32).powf(sign as f32);
 
-    pub fn decode(sign: u32, exponent: u32, fraction: u32) -> (f32, f32, f32) {
-        let signed_1 = (-1.0_f32).powf(sign as f32);
-        let exponent = (exponent as i32) - Self::BIAS;
-        let exponent = Self::RADIX.powf(exponent as f32);
+    let exponent = (exponent as i32) - BIAS;
+    let exponent = RADIX.powf(exponent as f32);
 
-        let mut mantissa: f32 = 1.0;
+    let mut mantissa: f32 = 1.0;
 
-        for i in 0..23 {
-            let mask = 1 << i;
-            let one_at_bit_i = fraction & mask;
-            if one_at_bit_i != 0 {
-                let i_ = i as f32;
-                let weight = 2_f32.powf(i_ - 23.0);
-                mantissa += weight;
-            }
+    for i in 0..23 {
+        let mask = 1 << i;
+        let one_at_bit_i = fraction & mask;
+        if one_at_bit_i != 0 {
+            let i_ = i as f32;
+            let weight = 2_f32.powf(i_ - 23.0);
+            mantissa += weight;
         }
-
-        (signed_1, exponent, mantissa)
     }
 
-    fn to_parts(n: f32) -> (u32, u32, u32) {
-        let bits = n.to_bits();
-
-        let sign = (bits >> 31) & 1;
-        let exponent = (bits >> 23) & 0xff;
-        let fraction = bits & 0x7fffff;
-
-        (sign, exponent, fraction)
-    }
-
-    fn from_parts(sign: f32, exponent: f32, mantissa: f32) -> f32 {
-        sign * exponent * mantissa
-    }
+    (signed_1, exponent, mantissa)
 }
